6b3f718eed556186e89c026c349e29c80ef80232
Thomas <thomasplatschorre@gmail.com>
2022-04-13 23:26:01 +0200

    changed thread interval

--- main.cpp
+++ main.cpp
@@ -148,7 +148,7 @@ int WinMain(
 					break;
 
 				printf("[INF] Waiting for combo box to close...\n");
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
 			}
 
 			printf("[INF] Scanning for processes\n");
